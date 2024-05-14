3a67b1e481e4d14c73c6b0fdde87eca2321d57f7
Carlos <cadanielmarchesin@gmail.com>
2024-05-13 21:39:04 -0300

    acessibilidade

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,13 @@ function toggleMenu() {
     if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
+    }
 }
 
 menuBtn.addEventListener('click', toggleMenu);
