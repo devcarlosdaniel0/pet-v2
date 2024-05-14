c5f98eccd437d8c9bfbde64458b725c67637830e
Carlos <cadanielmarchesin@gmail.com>
2024-05-13 21:28:49 -0300

    criação do btn

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
 const menuBtn = document.getElementById('btn-mobile');
 
 function toggleMenu() {
+    if (event.type === 'touchstart') event.preventDefault();
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
 
 menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener('touchstart', toggleMenu);
